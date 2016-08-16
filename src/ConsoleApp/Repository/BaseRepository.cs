79c441be128de0ba958f0763fe5bd03365e79a56
Hem Talreja <HemTalreja@gmail.com>
2016-08-16 15:07:34 -0400

    Changed the configuration of the abstract repository.

--- src/ConsoleApp/Repository/BaseRepository.cs
+++ src/ConsoleApp/Repository/BaseRepository.cs
@@ -1,22 +1,19 @@
-﻿using SimpleNet.Core.Data.Repository;
+﻿using System;
+using SimpleNet.Core.Data;
+using SimpleNet.Core.Data.Repository;
 using SimpleNet.Core.Data.SqlServer;
 
 namespace ConsoleApp.Repository
 {
     public class BaseRepository : AbstractSimpleRepository
     {
-        static string CONNECTION_STRING = string.Empty;
-
-        static BaseRepository()
+        public override ISimpleDataAccessLayer Database { get; }
+        
+        public BaseRepository()
         {
-            CONNECTION_STRING = @"Server=.\SQLEXPRESS;Database=AdventureWorks2012;Trusted_Connection=True;";
-        }
+            const string CONNECTION_STRING = @"Server=.\SQLEXPRESS;Database=AdventureWorks2012;Trusted_Connection=True;";
 
-
-        // Yes! I know the connection string should be injected...
-        protected BaseRepository() : base(new SqlServerProvider(CONNECTION_STRING))
-        {
-
+            Database = new SimpleDataAccessLayer(new SqlServerProvider(CONNECTION_STRING));
         }
     }
 }
--- src/SimpleNet.Core.Data/Repository/AbstractSimpleRepository.cs
+++ src/SimpleNet.Core.Data/Repository/AbstractSimpleRepository.cs
@@ -9,27 +9,17 @@ namespace SimpleNet.Core.Data.Repository
 {
     public abstract class AbstractSimpleRepository
     {
+        public abstract ISimpleDataAccessLayer Database { get; }
 
-        private readonly ISimpleDataAccessLayer _db;
-
-
-        protected AbstractSimpleRepository(ISimpleDatabaseProvider provider)
-                        :this(new SimpleDataAccessLayer(provider))
-        {
-        }
-        protected AbstractSimpleRepository(ISimpleDataAccessLayer db)
-        {
-            _db = db;
-        }
-
+        
 
         protected DbParameter GetParameter(string name, object value)
         {
-            return _db.DatabaseProvider.GetParameter(name, value);
+            return Database.DatabaseProvider.GetParameter(name, value);
         }
         protected DbParameter GetParameter(string name, object value, ParameterDirection direction)
         {
-            return _db.DatabaseProvider.GetParameter(name, value, direction);
+            return Database.DatabaseProvider.GetParameter(name, value, direction);
         }
 
 
@@ -37,35 +27,35 @@ namespace SimpleNet.Core.Data.Repository
         protected async Task<IList<T>> ReadAsync<T>(IRowMapper<T> mapper, string commandText, 
                                                 CommandType commandType, DbParameter[] parameters)
         {
-            return await _db.ReadAsync(mapper, commandText, commandType, parameters);
+            return await Database.ReadAsync(mapper, commandText, commandType, parameters);
         }
 
         protected async Task<IList<T>> ReadAsync<T>(DbConnection connection, IRowMapper<T> mapper, string commandText, 
                                                 CommandType commandType, DbParameter[] parameters, DbTransaction transaction = null)
         {
-            return await _db.ReadAsync(connection, mapper, commandText, commandType, parameters, transaction);
+            return await Database.ReadAsync(connection, mapper, commandText, commandType, parameters, transaction);
         }
 
         protected async Task<int> ExecuteNonQueryAsync(string commandText, CommandType commandType, DbParameter[] parameters)
         {
-            return await _db.ExecuteNonQueryAsync(commandText, commandType, parameters);
+            return await Database.ExecuteNonQueryAsync(commandText, commandType, parameters);
         }
 
         protected async Task<int> ExecuteNonQueryAsync(    DbConnection connection, string commandText, CommandType commandType, 
                                                         DbParameter[] parameters, DbTransaction transaction = null )
         {
-            return await _db.ExecuteNonQueryAsync(connection, commandText, commandType, parameters, transaction);
+            return await Database.ExecuteNonQueryAsync(connection, commandText, commandType, parameters, transaction);
         }
 
         protected async Task<object> ExecuteScalarAsync(string commandText, CommandType commandType, DbParameter[] parameters)
         {
-            return await _db.ExecuteScalarAsync(commandText, commandType, parameters);
+            return await Database.ExecuteScalarAsync(commandText, commandType, parameters);
         }
 
         protected async Task<object> ExecuteScalarAsync(   DbConnection connection, string commandText, CommandType commandType, 
                                                         DbParameter[] parameters, DbTransaction transaction = null)
         {
-            return await _db.ExecuteScalarAsync(connection, commandText, commandType, parameters, transaction);
+            return await Database.ExecuteScalarAsync(connection, commandText, commandType, parameters, transaction);
         }
     }
 }
